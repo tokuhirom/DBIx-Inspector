594bef90278b1eaa802b61d2b445c182acc5e702
hatyuki29 <hatyuki29@gmail.com>
2012-07-17 20:43:14 +0900

    hash keys should be upper case

--- lib/DBIx/Inspector/Table/Pg.pm
+++ lib/DBIx/Inspector/Table/Pg.pm
@@ -7,8 +7,8 @@ use base qw/DBIx::Inspector::Table/;
 # They return unquoted name.
 # DBD::Pg v1.xx does not support these attribues,
 # but don't you use such a old module, do you?
-sub name   { $_[0]->{ pg_table } }
-sub schema { $_[0]->{ pg_schema } }
+sub name   { $_[0]->{ PG_TABLE } }
+sub schema { $_[0]->{ PG_SCHEMA } }
 
 1;
 
