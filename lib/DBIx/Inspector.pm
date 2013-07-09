30b0e3fda23dfab6530cedfda13c2de69968a856
tokuhirom <tokuhirom@gmail.com>
2013-07-09 12:14:58 +0900

    Checking in changes prior to tagging of version 0.12.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 928dcca..023ee8f 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension DBIx::Inspector
    
     {{$NEXT}}
    
    +0.12 2013-07-09T03:14:37Z
    +
    +    - minil migrate
    +
     0.11 2013-02-25T10:41:13
    
         - removed experimental feature introduced in 0.10.

--- lib/DBIx/Inspector.pm
+++ lib/DBIx/Inspector.pm
@@ -2,7 +2,7 @@ package DBIx::Inspector;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 use Class::Accessor::Lite;
 Class::Accessor::Lite->mk_accessors(qw/dbh catalog schema driver/);
 use Carp ();
